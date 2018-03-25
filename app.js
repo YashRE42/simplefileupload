44d80f44e8bdbf5f750fa2ae8466a70850a9667d
Yash Rathore <yrabc123@gmail.com>
2018-03-25 17:14:24 +0000

    Customize template and complete app

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({
-  dest: 'uploads/' // this saves your file into a directory called "uploads"
-}); 
-
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
+let fileName;
+
+app.set("view engine","ejs");
+
+app.get("/", (req, res) => {
+  res.send("Your form is on /file/new, you can get a file at /file/<your-file-id>");
+});
+
+app.get('/file/new', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// It's very crucial that the file name matches the name attribute in your html
-app.post('/', upload.single('file-to-upload'), (req, res) => {
-  res.redirect('/');
+app.get("/file/:id", (req, res) => {
+  res.sendFile(__dirname + '/uploads/' + req.params.id);
 });
 
-app.listen(process.env.PORT,process.env.IP, function(){
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads'); //file will be stored in directory called uploads on server
+  },
+  filename: function (req, file, cb) {
+    fileName = Date.now() + path.extname(file.originalname);
+    cb(null, fileName);
+  }
+});
+ 
+const upload = multer({ storage: storage });
+
+// It's very crucial that the file name matches the name attribute in your html
+app.post('/file/', upload.single('file-to-upload'), (req, res) => {
+    res.send(fileName);
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started.");
 });
